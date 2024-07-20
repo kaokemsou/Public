4569aa320f8b704dab1b79508e9f26f09c92000a
kaokemsou <kemsourkao@gmail.com>
2024-07-19 23:19:57 -0400

    firt test

---
+++ app.py
@@ -0,0 +1,35 @@
+# print("Hello")
+# print("single 'quote' ")
+# print('double "quote"')
+# print("single\'quote\' and \"dkjdf\" ")
+
+# string
+# print("Hello "+" From btb" +str(5))
+
+# name = input("What is your name:")
+# print("Welcome " + name)
+# print("Welcome amount cal")
+# qty1=int(input("qty1:"))
+# qty2=int(input("qty2:"))
+# price=float(input("price:"))
+# total_qty=qty1 + qty2
+# print("Total Qty:" +str(total_qty))
+# amount=total_qty*price
+# print("Amount:" +str(amount))
+
+
+####################
+
+# a = input("a")
+# b = input("b")
+# c=a
+# a=b
+# b=c
+# print("a: " +a)
+# print("b: " +b)
+
+
+##################
+
+print("Welcome to the band name generator")
+city = input(print("Which city did you grow up in? \n"))
